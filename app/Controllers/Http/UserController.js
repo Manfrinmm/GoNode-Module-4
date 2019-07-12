6ed52835bd2f2631c704e6432d4156e066a45c8e
Manfrinmm <matheus_mm123@hotmail.com>
2019-07-12 02:29:43 -0300

    Add transaction no UserController

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const Database = use("Database");
 const User = use("App/models/User");
 
 class UserController {
@@ -7,10 +8,16 @@ class UserController {
     const data = request.only(["username", "email", "password"]);
     const addresses = request.input("addresses");
 
-    const user = await User.create(data);
+    //caso falhe em algum. ele executa um rollback em tudo que estiver usando trx
+    const trx = await Database.beginTransaction(); //se utiliza quando há mais de uma operação no BD, garatindo sucesso
+
+    const user = await User.create(data, trx);
 
     //ira criar varios endereços
-    await user.addresses().createMany(addresses);
+    await user.addresses().createMany(addresses, trx);
+
+    //se ele chegou até aqui sem nenhum erro, ira efetuar as alteções no BD
+    await trx.commit();
 
     return user;
   }
