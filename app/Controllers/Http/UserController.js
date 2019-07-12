c0ae41f977f2ca432c95dfd99d7f736d054c3bab
Manfrinmm <matheus_mm123@hotmail.com>
2019-07-12 02:22:48 -0300

    Salvando relacionamento Users e Addresses

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -5,8 +5,12 @@ const User = use("App/models/User");
 class UserController {
   async store({ request }) {
     const data = request.only(["username", "email", "password"]);
+    const addresses = request.input("addresses");
 
     const user = await User.create(data);
+
+    //ira criar varios endereços
+    await user.addresses().createMany(addresses);
 
     return user;
   }
